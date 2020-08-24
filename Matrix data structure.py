64545dc58a12f7d8b6156572d83428c48613131b
Karni Bedirian <39968446+KHBHH@users.noreply.github.com>
2020-08-24 19:01:49 +0400

    Update Matrix data structure.py

--- Matrix data structure.py
+++ Matrix data structure.py
@@ -7,3 +7,12 @@ class Matrix:
                 rowline.append(int(input()))
             mtrx.append(rowline) 
         return mtrx
+    
+    def trans(m):
+        dim_row = len(m)
+        dim_col = len(m[0])
+        trans_M =[[0 for i in range(dim_row)] for j in range(dim_col)]
+        for i in range(dim_row):
+            for j in range(dim_col):
+                trans_M[j][i]= m[i][j]
+        return trans_M
