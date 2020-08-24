b95743459c08ffd86e81e96c88c4ce6660c59311
Karni Bedirian <39968446+KHBHH@users.noreply.github.com>
2020-08-24 22:14:40 +0400

    Update Matrix data structure.py

--- Matrix data structure.py
+++ Matrix data structure.py
@@ -1,5 +1,6 @@
 class Matrix:
     
+    #function to create matrix of size rowNum x colNum
     def matrix(self,rowNum, colNum):
         mtrx = []
         for i in range(rowNum):           
@@ -9,6 +10,7 @@ class Matrix:
             mtrx.append(rowline) 
         return mtrx
     
+    #function to multiply matrix by scalar
     def scal_mult(self,matrix, scalar):
         result = matrix
         for i in range(len(matrix)):
@@ -18,12 +20,11 @@ class Matrix:
     
     #multiplication of 2 matrices
     def matrix_mult(self,A,B):
-    #check first if multiplication is possible
         rows_A = len(A)
         columns_A = len(A[0])
         rows_B = len(B)
         columns_B = len(B[0])
-        if (columns_A != rows_B):
+        if (columns_A != rows_B):   #check first if multiplication is possible
             raise ArithmeticError('Invalid dimensions, can not do multiplication')
         else:
             result = [[0 for i in range(columns_B)] for j in range(rows_A)]
@@ -35,6 +36,7 @@ class Matrix:
                     result[i][j] = temp
         return result
     
+    #function to find the transpose of matrix m
     def transpose(self, m):
         dim_row = len(m)
         dim_col = len(m[0])
@@ -44,6 +46,7 @@ class Matrix:
                 trans_M[j][i]= m[i][j]
         return trans_M
     
+    #function to get the sum of diagonals of matrix m
     def trace(self, m):
         summation = 0
         j=0
